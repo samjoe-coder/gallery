75e2384d1161119eb4b8a1a72e603aac49c16b0a
Samuel Joseph Karetla <ksamueljoseph006@gmail.com>
2025-08-30 23:30:09 +0530

    Hono setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,9 @@
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/hello', (c) => {
+    return c.json({ message: 'Hello Sam!' });
+});
+
+export default app;
