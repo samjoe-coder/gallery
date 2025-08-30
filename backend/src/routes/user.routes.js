4748b9e4fde5ca0754547180d6ea932d4b6cf037
Samuel Joseph Karetla <ksamueljoseph006@gmail.com>
2025-08-31 02:12:36 +0530

    "ReferenceError: env is not defined"  fix

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -3,7 +3,7 @@ import { Hono } from 'hono'
 const userRouter = new Hono()
 
 userRouter.get('/', async (c) => {
-    let { results } = await env.DB.prepare(
+    let { results } = await c.env.DB.prepare(
         "SELECT * FROM users",
       ).run();
     return c.json({ message: 'GET all users', data: results });
@@ -13,10 +13,10 @@ userRouter.get('/:id', async (c) => {return c.json({ message: 'GET user details'
 
 userRouter.post('/', async (c) => {
 
-    let { results } = await evn.DB.prepare(
+    let { results } = await c.env.DB.prepare(
         `INSERT INTO users (id, email, password, username, avatar_url)
  VALUES ('random-uuid', 'alice@example.com', 'securepass', 'alice', 'https://example.com/alice.png');`
-    ).run
+    ).run();
 
     return c.json({ message: 'Create new user' , data : results});
 
